b4174efd8ebbeddce4b793b6a59a5e1b8102330b
Kentaro Hayashi <hayashi@clear-code.com>
2021-09-03 14:45:31 +0900

    Add configuration parameters
    Signed-off-by: Kentaro Hayashi <hayashi@clear-code.com>

--- lib/fluent/plugin/in_node_exporter_metrics.rb
+++ lib/fluent/plugin/in_node_exporter_metrics.rb
@@ -13,12 +13,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require "cmetrics"
 require "fluent/plugin/input"
 
 module Fluent
   module Plugin
     class NodeExporterMetricsInput < Fluent::Plugin::Input
       Fluent::Plugin.register_input("node_exporter_metrics", self)
+
+      helpers :timer
+
+      desc "Interval to scrape metrics from node"
+      config_param :scrape_interval, :time, default: 5
+      desc "Path to mount point to collect process information and metrics"
+      config_param :procfs_path, :string, default: "/proc"
+      desc "Path to file-system to collect system metrics"
+      config_param :sysfs_path, :string, default: "/sys"
+
+      desc "Enable cpu collector"
+      config_param :cpu, :bool, default: true
+      desc "Enable cpu collector"
+      config_param :cpufreq, :bool, default: true
+      desc "Enable diskstats collector"
+      config_param :diskstats, :bool, default: true
+      desc "Enable filefd collector"
+      config_param :filefd, :bool, default: true
+      desc "Enable loadavg collector"
+      config_param :loadavg, :bool, default: true
+      desc "Enable meminfo collector"
+      config_param :meminfo, :bool, default: true
+      desc "Enable netdev collector"
+      config_param :netdev, :bool, default: true
+      desc "Enable stat collector"
+      config_param :stat, :bool, default: true
+      desc "Enable time collector"
+      config_param :time, :bool, default: true
+      desc "Enable uname collector"
+      config_param :uname, :bool, default: true
+      desc "Enable vmstat collector"
+      config_param :vmstat, :bool, default: true
+
     end
   end
 end
