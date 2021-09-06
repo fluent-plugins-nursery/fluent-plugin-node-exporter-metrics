4a8f2a8166f476dbc79278696e480932d7567074
Kentaro Hayashi <hayashi@clear-code.com>
2021-09-06 14:21:33 +0900

    Add parameter test case

--- test/helper.rb
+++ test/helper.rb
@@ -3,6 +3,7 @@ require "test-unit"
 require "fluent/test"
 require "fluent/test/driver/input"
 require "fluent/test/helpers"
+require "fluent/plugin/in_node_exporter_metrics"
 
 Test::Unit::TestCase.include(Fluent::Test::Helpers)
 Test::Unit::TestCase.extend(Fluent::Test::Helpers)
--- test/plugin/test_in_node_exporter_metrics.rb
+++ test/plugin/test_in_node_exporter_metrics.rb
@@ -6,13 +6,45 @@ class NodeExporterMetricsInputTest < Test::Unit::TestCase
     Fluent::Test.setup
   end
 
-  test "failure" do
-    flunk
+  teardown do
+    Fluent::Engine.stop
   end
 
-  private
+  CONFIG = config_element("ROOT", "", {
+                            "scrape_interval" => 5,
+                            "procfs_path" => "/proc",
+                            "sysfs_path" => "/sys"
+                          })
 
-  def create_driver(conf)
+  DEFAULT_COLLECTORS = CONFIG + config_element("", "", {
+                                               })
+
+  def create_driver(conf = CONFIG)
     Fluent::Test::Driver::Input.new(Fluent::Plugin::NodeExporterMetricsInput).configure(conf)
+  end
+
+
+  sub_test_case "configure" do
+    def test_default_parameters
+      d = create_driver(CONFIG)
+      assert_equal([5, "/proc", "/sys"],
+                   [d.instance.scrape_interval, d.instance.procfs_path, d.instance.sysfs_path])
+    end
+
+    def test_default_collectors
+      d = create_driver(CONFIG)
+      assert_equal([true] * 11,
+                   [d.instance.cpu,
+                    d.instance.cpufreq,
+                    d.instance.diskstats,
+                    d.instance.filefd,
+                    d.instance.loadavg,
+                    d.instance.meminfo,
+                    d.instance.netdev,
+                    d.instance.stat,
+                    d.instance.time,
+                    d.instance.uname,
+                    d.instance.vmstat])
+    end
   end
 end
