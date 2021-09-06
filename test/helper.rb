db7ab81e7afa089fdd61611ae6578eeff21cdfc0
Kentaro Hayashi <hayashi@clear-code.com>
2021-09-06 15:47:16 +0900

    Add helper method to access fixture

--- test/helper.rb
+++ test/helper.rb
@@ -7,3 +7,17 @@ require "fluent/plugin/in_node_exporter_metrics"
 
 Test::Unit::TestCase.include(Fluent::Test::Helpers)
 Test::Unit::TestCase.extend(Fluent::Test::Helpers)
+
+def fixture_filesystem_root(collector, test_case, filesystem)
+  File.expand_path(File.join(File.dirname(__FILE__), "fixtures",
+                             collector, test_case, filesystem))
+end
+
+def fixture_sysfs_root(collector, test_case)
+  fixture_filesystem_root(collector, test_case, "sys")
+end
+
+def fixture_procfs_root(collector, test_case)
+  fixture_filesystem_root(collector, test_case, "proc")
+end
+
