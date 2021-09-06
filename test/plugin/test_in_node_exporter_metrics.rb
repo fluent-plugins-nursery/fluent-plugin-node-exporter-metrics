96fb002b619965243a0fbcb6040d6464b39d02c0
Kentaro Hayashi <hayashi@clear-code.com>
2021-09-06 18:00:30 +0900

    test: check customizable parameters

--- test/plugin/test_in_node_exporter_metrics.rb
+++ test/plugin/test_in_node_exporter_metrics.rb
@@ -46,5 +46,39 @@ class NodeExporterMetricsInputTest < Test::Unit::TestCase
                     d.instance.uname,
                     d.instance.vmstat])
     end
+
+    def test_customizable
+      d = create_driver(config_element("ROOT", "", {
+                                                  "scrape_interval" => 10,
+                                                  "procfs_path" => "/proc/dummy",
+                                                  "sysfs_path" => "/sys/dummy",
+                                                  "cpu" => "false",
+                                                  "cpufreq" => "false",
+                                                  "diskstats" => "false",
+                                                  "filefd" => "false",
+                                                  "loadavg" => "false",
+                                                  "meminfo" => "false",
+                                                  "netdev" => "false",
+                                                  "stat" => "false",
+                                                  "time" => "false",
+                                                  "uname" => "false",
+                                                  "vmstat" => "false"
+                                                }))
+      assert_equal([10.0, "/proc/dummy", "/sys/dummy", *[false] * 11],
+                   [d.instance.scrape_interval,
+                    d.instance.procfs_path,
+                    d.instance.sysfs_path,
+                    d.instance.cpu,
+                    d.instance.cpufreq,
+                    d.instance.diskstats,
+                    d.instance.filefd,
+                    d.instance.loadavg,
+                    d.instance.meminfo,
+                    d.instance.netdev,
+                    d.instance.stat,
+                    d.instance.time,
+                    d.instance.uname,
+                    d.instance.vmstat])
+    end
   end
 end
