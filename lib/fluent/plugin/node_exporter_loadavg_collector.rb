11e918e4ede000eb9f2e0a1d0fd1067cb3533dc2
Kentaro Hayashi <hayashi@clear-code.com>
2021-09-07 13:21:14 +0900

    loadavg: add loadavg collector

---
+++ lib/fluent/plugin/node_exporter_loadavg_collector.rb
@@ -0,0 +1,62 @@
+#
+# Copyright 2021- Kentaro Hayashi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "cmetrics"
+require "fluent/plugin/input"
+require "fluent/plugin/node_exporter_collector"
+
+module Fluent
+  module Plugin
+    class NodeExporterLoadavgMetricsCollector < NodeExporterMetricsCollector
+      def initialize(config={})
+        super(config)
+
+        @load1 = CMetrics::Gauge.new
+        @load1.create("node", "", "load1", "1m load average.")
+
+        @load5 = CMetrics::Gauge.new
+        @load5.create("node", "", "load5", "5m load average.")
+
+        @load15 = CMetrics::Gauge.new
+        @load15.create("node", "", "load1", "15m load average.")
+      end
+
+      def run
+        loadavg_update
+      end
+
+      def loadavg_update
+        loadavg_path = File.join(@procfs_path, "/loadavg")
+        # Use 1 explicitly for default gauge value
+        fields = File.read(loadavg_path).split
+        unless fields.size == 5
+          $log.warn("invalid number of fields <#{loadavg_path}>: <#{fields.size}>")
+          return
+        end
+        @load1.set(fields[0].to_f)
+        @load5.set(fields[1].to_f)
+        @load15.set(fields[2].to_f)
+      end
+
+      def cmetrics
+        {
+          loadavg1: @load1,
+          loadavg5: @load5,
+          loadavg15: @load15
+        }
+      end
+    end
+  end
+end
---
+++ test/plugin/test_loadavg_collector.rb
@@ -0,0 +1,34 @@
+require "helper"
+require "fluent/plugin/in_node_exporter_metrics"
+require "fluent/plugin/node_exporter_loadavg_collector"
+
+class NodeExporterTimeColectorTest < Test::Unit::TestCase
+  sub_test_case "loadavg" do
+
+    def test_invalid_fields
+      config = {
+        procfs_path: fixture_procfs_root("loadavg", "invalid_fields")
+      }
+      collector = Fluent::Plugin::NodeExporterLoadavgMetricsCollector.new(config)
+      collector.run
+      loadavg1 = collector.cmetrics[:loadavg1]
+      loadavg5 = collector.cmetrics[:loadavg5]
+      loadavg15 = collector.cmetrics[:loadavg15]
+      assert_equal([0.0, 0.0, 0.0],
+                   [loadavg1.val, loadavg5.val, loadavg15.val])
+    end
+
+    def test_valid_fields
+      config = {
+        procfs_path: fixture_procfs_root("loadavg", "valid_fields")
+      }
+      collector = Fluent::Plugin::NodeExporterLoadavgMetricsCollector.new(config)
+      collector.run
+      loadavg1 = collector.cmetrics[:loadavg1]
+      loadavg5 = collector.cmetrics[:loadavg5]
+      loadavg15 = collector.cmetrics[:loadavg15]
+      assert_equal([0.10, 0.20, 0.30],
+                   [loadavg1.val, loadavg5.val, loadavg15.val])
+    end
+  end
+end
