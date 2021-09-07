d10e2c644fd105674b10abfbb5f092f1c0e25068
Kentaro Hayashi <hayashi@clear-code.com>
2021-09-07 13:21:35 +0900

    filefd: add filefd collector

---
+++ lib/fluent/plugin/node_exporter_filefd_collector.rb
@@ -0,0 +1,60 @@
+#
+# Copyright 2021- Kentaro Hayashi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "cmetrics"
+require "etc"
+require "resolv"
+require "fluent/plugin/input"
+require "fluent/plugin/node_exporter_collector"
+
+module Fluent
+  module Plugin
+    class NodeExporterFilefdMetricsCollector < NodeExporterMetricsCollector
+      def initialize(config={})
+        super(config)
+
+        @allocated = CMetrics::Gauge.new
+        @allocated.create("node", "filefd", "allocated", "File descriptor statistics: allocated.")
+
+        @maximum = CMetrics::Gauge.new
+        @maximum.create("node", "filefd", "maximum", "File descriptor statistics: maximum.")
+      end
+
+      def run
+        filefd_update
+      end
+
+      def filefd_update
+        # Etc.uname returns at least sysname,release,version,machine,nodename
+        # but it is not guaranteed to return domainname.
+        file_nr_path = File.join(@procfs_path, "/sys/fs/file-nr")
+        entry = File.read(file_nr_path).split
+        unless entry.size == 3
+          $log.warn("invalid number of field <#{file_nr_path}>: #{entry.size}")
+          return
+        end
+        @allocated.set(entry.first.to_f)
+        @maximum.set(entry.last.to_f)
+      end
+
+      def cmetrics
+        {
+          filefd_allocated: @allocated,
+          filefd_maximum: @maximum
+        }
+      end
+    end
+  end
+end
---
+++ test/plugin/test_filefd_collector.rb
@@ -0,0 +1,32 @@
+require "helper"
+require "fluent/plugin/in_node_exporter_metrics"
+require "fluent/plugin/node_exporter_filefd_collector"
+
+class NodeExporterTimeColectorTest < Test::Unit::TestCase
+  sub_test_case "filefd" do
+
+    def test_invalid_fields
+      config = {
+        procfs_path: fixture_procfs_root("filefd", "invalid_fields")
+      }
+      collector = Fluent::Plugin::NodeExporterFilefdMetricsCollector.new(config)
+      collector.run
+      allocated = collector.cmetrics[:filefd_allocated]
+      maximum = collector.cmetrics[:filefd_maximum]
+      assert_equal([0.0, 0.0],
+                   [allocated.val, maximum.val])
+    end
+
+    def test_valid_fields
+      config = {
+        procfs_path: fixture_procfs_root("filefd", "valid_fields")
+      }
+      collector = Fluent::Plugin::NodeExporterFilefdMetricsCollector.new(config)
+      collector.run
+      allocated = collector.cmetrics[:filefd_allocated]
+      maximum = collector.cmetrics[:filefd_maximum]
+      assert_equal([100.0, 10000.0],
+                   [allocated.val, maximum.val])
+    end
+  end
+end
