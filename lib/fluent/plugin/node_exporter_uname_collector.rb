f6fa7f737f3482a379b2fa29aad5fb0e5e98afb8
Kentaro Hayashi <hayashi@clear-code.com>
2021-09-07 11:35:30 +0900

    uname: add uname collector

---
+++ lib/fluent/plugin/node_exporter_uname_collector.rb
@@ -0,0 +1,63 @@
+#
+# Copyright 2021- Kentaro Hayashi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "cmetrics"
+require "etc"
+require "resolv"
+require "fluent/plugin/input"
+require "fluent/plugin/node_exporter_collector"
+
+module Fluent
+  module Plugin
+    class NodeExporterUnameMetricsCollector < NodeExporterMetricsCollector
+      def initialize(config={})
+        super(config)
+
+        @gauge = CMetrics::Gauge.new
+        @gauge.create("node", "uname", "info",
+                      "Labeled system information as provided by the uname system call.",
+                      ["sysname", "release", "version", "machine", "nodename", "domainname"])
+      end
+
+      def run
+        uname_update
+      end
+
+      def uname_update
+        # Etc.uname returns at least sysname,release,version,machine,nodename
+        # but it is not guaranteed to return domainname.
+        domainname = if Etc.uname.has_key?(:domainname)
+                       Etc.uname[:domainname]
+                     else
+                       "(none)"
+                     end
+        # Use 1 explicitly for default gauge value
+        @gauge.set(1, [
+                     Etc.uname[:sysname],
+                     Etc.uname[:release],
+                     Etc.uname[:version],
+                     Etc.uname[:machine],
+                     Etc.uname[:nodename],
+                     domainname])
+      end
+
+      def cmetrics
+        {
+          info: @gauge
+        }
+      end
+    end
+  end
+end
---
+++ test/plugin/test_uname_collector.rb
@@ -0,0 +1,40 @@
+require "helper"
+require "fluent/plugin/in_node_exporter_metrics"
+require "fluent/plugin/node_exporter_uname_collector"
+
+class NodeExporterTimeColectorTest < Test::Unit::TestCase
+  sub_test_case "info" do
+    WITHOUT_DOMAINNAME = {
+      sysname: "Linux",
+      release: "5.10.0-8-amd64",
+      version: "#1 SMP Debian 5.10.46-4 (2021-08-03)",
+      machine: "x86_64",
+      nodename: "jessie",
+    }
+
+    WITH_DOMAINNAME = {
+      sysname: "Linux",
+      release: "5.10.0-8-amd64",
+      version: "#1 SMP Debian 5.10.46-4 (2021-08-03)",
+      machine: "x86_64",
+      nodename: "jackie",
+      domainname: "marion"
+    }
+
+    def test_with_domainmame
+      collector = Fluent::Plugin::NodeExporterUnameMetricsCollector.new
+      stub(Etc).uname { WITH_DOMAINNAME }
+      collector.run
+      info = collector.cmetrics[:info]
+      assert_equal(1, info.val(WITH_DOMAINNAME.values))
+    end
+
+    def test_without_domainmame
+      collector = Fluent::Plugin::NodeExporterUnameMetricsCollector.new
+      stub(Etc).uname { WITHOUT_DOMAINNAME }
+      collector.run
+      info = collector.cmetrics[:info]
+      assert_equal(1, info.val(WITHOUT_DOMAINNAME.values << "(none)"))
+    end
+  end
+end
