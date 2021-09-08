fd4bfbfc0ab58d1ef7173016a89786f760fb0d6f
Kentaro Hayashi <hayashi@clear-code.com>
2021-09-08 15:40:27 +0900

    netdev: add netdev collector

---
+++ lib/fluent/plugin/node_exporter/netdev_collector.rb
@@ -0,0 +1,74 @@
+#
+# Copyright 2021- Kentaro Hayashi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "cmetrics"
+require "fluent/plugin/input"
+require "fluent/plugin/node_exporter/collector"
+
+module Fluent
+  module Plugin
+    module NodeExporter
+      class NetdevMetricsCollector < MetricsCollector
+
+        def initialize(config={})
+          super(config)
+
+          @metrics = {}
+        end
+
+        def run
+          netdev_update
+        end
+
+        RECEIVE_FIELDS = %w(bytes packets errs drop fifo frame compressed multicast)
+        TRANSMIT_FIELDS = %w(bytes packets errs drop fifo colls carrier compressed)
+
+        def netdev_update
+          netdev_path = File.join(@procfs_path, "net/dev")
+          File.readlines(netdev_path).each_with_index do |line, index|
+            # net/dev must be 3 columns
+            if index == 0 and line.split("|").size != 3
+              break
+            end
+            # first 2 line are header (Inter-face/Receive/Transmit)
+            next if index < 2
+
+            interface, *values = line.split
+            interface.delete!(":")
+
+            RECEIVE_FIELDS.each_with_index do |field, index|
+              metric_name = "receive_#{field}_total"
+              @counter = CMetrics::Counter.new
+              @counter.create("node", "network", metric_name, "Network device statistic #{interface}.", ["device"])
+              @counter.set(values[index].to_f, [interface])
+              @metrics[metric_name] = @counter
+            end
+            TRANSMIT_FIELDS.each_with_index do |field, index|
+              metric_name = "transmit_#{field}_total"
+              @counter = CMetrics::Counter.new
+              @counter.create("node", "network", metric_name, "Network device statistic #{interface}.", ["device"])
+              @counter.set(values[index + RECEIVE_FIELDS.size].to_f, [interface])
+              @metrics[metric_name] = @counter
+            end
+          end
+        end
+
+        def cmetrics
+          @metrics
+        end
+      end
+    end
+  end
+end
---
+++ test/plugin/test_netdev_collector.rb
@@ -0,0 +1,35 @@
+require "helper"
+require "fluent/plugin/in_node_exporter_metrics"
+require "fluent/plugin/node_exporter/netdev_collector"
+
+class NodeExporterTimeColectorTest < Test::Unit::TestCase
+  sub_test_case "netdev" do
+
+    def parse(input)
+      stub(File).readlines { input.split("\n") }
+      collector = Fluent::Plugin::NodeExporter::NetdevMetricsCollector.new
+      collector.run
+      yield collector
+    end
+
+    def test_netdev_lo
+      proc_netdev = <<EOS
+Inter-|   Receive                                                |  Transmit
+ face |bytes    packets errs drop fifo frame compressed multicast|bytes    packets errs drop fifo colls carrier compressed
+    lo:    1          2    3    4    5     6          7         8     9         10   11   12   13    14      15         16
+EOS
+      parse(proc_netdev) do |collector|
+        expected = 16.times.collect do |i| (i + 1).to_f end
+
+        values = []
+        Fluent::Plugin::NodeExporter::NetdevMetricsCollector::RECEIVE_FIELDS.each do |field|
+          values << collector.cmetrics["receive_#{field}_total"].val(["lo"])
+        end
+        Fluent::Plugin::NodeExporter::NetdevMetricsCollector::TRANSMIT_FIELDS.each do |field|
+          values << collector.cmetrics["transmit_#{field}_total"].val(["lo"])
+        end
+        assert_equal(expected, values)
+      end
+    end
+  end
+end
