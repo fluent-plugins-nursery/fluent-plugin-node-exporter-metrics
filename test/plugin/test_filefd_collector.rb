f75cc2e64a488bf6af24d76febfc296cdc68d522
Kentaro Hayashi <hayashi@clear-code.com>
2021-09-08 18:11:34 +0900

    filefd: simplify

--- test/plugin/test_filefd_collector.rb
+++ test/plugin/test_filefd_collector.rb
@@ -2,31 +2,38 @@ require "helper"
 require "fluent/plugin/in_node_exporter_metrics"
 require "fluent/plugin/node_exporter/filefd_collector"
 
-class NodeExporterTimeColectorTest < Test::Unit::TestCase
+class NodeExporterFilefdColectorTest < Test::Unit::TestCase
   sub_test_case "filefd" do
 
-    def test_invalid_fields
-      config = {
-        procfs_path: fixture_procfs_root("filefd", "invalid_fields")
-      }
-      collector = Fluent::Plugin::NodeExporter::FilefdMetricsCollector.new(config)
+    def parse(input)
+      stub(File).read { input }
+      collector = Fluent::Plugin::NodeExporter::FilefdMetricsCollector.new
       collector.run
-      allocated = collector.cmetrics[:filefd_allocated]
-      maximum = collector.cmetrics[:filefd_maximum]
-      assert_equal([0.0, 0.0],
-                   [allocated.val, maximum.val])
+      yield collector
+    end
+
+    def test_invalid_fields
+      proc_filefd = <<EOS
+100	0	10000	1
+EOS
+      parse(proc_filefd) do |collector|
+        allocated = collector.cmetrics[:filefd_allocated]
+        maximum = collector.cmetrics[:filefd_maximum]
+        assert_equal([0.0, 0.0],
+                     [allocated.val, maximum.val])
+      end
     end
 
     def test_valid_fields
-      config = {
-        procfs_path: fixture_procfs_root("filefd", "valid_fields")
-      }
-      collector = Fluent::Plugin::NodeExporter::FilefdMetricsCollector.new(config)
-      collector.run
-      allocated = collector.cmetrics[:filefd_allocated]
-      maximum = collector.cmetrics[:filefd_maximum]
-      assert_equal([100.0, 10000.0],
-                   [allocated.val, maximum.val])
+      proc_filefd = <<EOS
+100	0	10000
+EOS
+      parse(proc_filefd) do |collector|
+        allocated = collector.cmetrics[:filefd_allocated]
+        maximum = collector.cmetrics[:filefd_maximum]
+        assert_equal([100.0, 10000.0],
+                     [allocated.val, maximum.val])
+      end
     end
   end
 end
