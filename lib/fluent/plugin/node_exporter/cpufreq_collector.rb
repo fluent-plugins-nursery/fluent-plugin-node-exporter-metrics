46fec504a9e28ec0ec687e82400b84e5d483ca1a
Kentaro Hayashi <hayashi@clear-code.com>
2021-09-08 17:59:59 +0900

    cpufreq: add cpufreq collector

---
+++ lib/fluent/plugin/node_exporter/cpufreq_collector.rb
@@ -0,0 +1,90 @@
+#
+# Copyright 2021- Kentaro Hayashi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "cmetrics"
+require "etc"
+require "fluent/plugin/input"
+require "fluent/plugin/node_exporter/collector"
+
+module Fluent
+  module Plugin
+    module NodeExporter
+      class CpufreqMetricsCollector < MetricsCollector
+        def initialize(config={})
+          super(config)
+
+          unless Fluent.windows?
+            @frequency_hertz = CMetrics::Gauge.new
+            @frequency_hertz.create("node", "cpu", "frequency_hertz",
+                                    "Current cpu thread frequency in hertz.", ["cpu"])
+
+            @frequency_max_hertz = CMetrics::Gauge.new
+            @frequency_max_hertz.create("node", "cpu", "frequency_max_hertz",
+                                       "Maximum cpu thread frequency in hertz.", ["cpu"])
+
+            @frequency_min_hertz = CMetrics::Gauge.new
+            @frequency_min_hertz.create("node", "cpu", "frequency_min_hertz",
+                                       "Minimum cpu thread frequency in hertz.", ["cpu"])
+
+            @scaling_frequency_hertz = CMetrics::Gauge.new
+            @scaling_frequency_hertz.create("node", "cpu", "scaling_frequency_hertz",
+                                            "Current scaled CPU thread frequency in hertz.", ["cpu"])
+
+            @scaling_frequency_max_hertz = CMetrics::Gauge.new
+            @scaling_frequency_max_hertz.create("node", "cpu", "scaling_frequency_max_hertz",
+                                       "Maximum scaled CPU thread frequency in hertz.", ["cpu"])
+
+            @scaling_frequency_min_hertz = CMetrics::Gauge.new
+            @scaling_frequency_min_hertz.create("node", "cpu", "scaling_frequency_min_hertz",
+                                       "Minimum scaled CPU thread frequency in hertz.", ["cpu"])
+          end
+        end
+
+        def run
+          cpufreq_update
+        end
+
+        def cpufreq_update
+          scan_sysfs_path("devices/system/cpu/cpu[0-9]*").each do |path|
+            cpuinfo_cur_freq_path = File.join(path, "cpufreq", "cpuinfo_cur_freq")
+            cpuinfo_max_freq_path = File.join(path, "cpufreq", "cpuinfo_max_freq")
+            cpuinfo_min_freq_path = File.join(path, "cpufreq", "cpuinfo_min_freq")
+            scaling_cur_freq_path = File.join(path, "cpufreq", "scaling_cur_freq")
+            scaling_max_freq_path = File.join(path, "cpufreq", "scaling_max_freq")
+            scaling_min_freq_path = File.join(path, "cpufreq", "scaling_min_freq")
+            cpu_id = File.basename(path).sub(/cpu(\d+)/, "\\1")
+            @frequency_hertz.set(File.read(cpuinfo_cur_freq_path).to_f, [cpu_id])
+            @frequency_max_hertz.set(File.read(cpuinfo_max_freq_path).to_f, [cpu_id])
+            @frequency_min_hertz.set(File.read(cpuinfo_min_freq_path).to_f, [cpu_id])
+            @scaling_frequency_hertz.set(File.read(scaling_cur_freq_path).to_f, [cpu_id])
+            @scaling_frequency_max_hertz.set(File.read(scaling_max_freq_path).to_f, [cpu_id])
+            @scaling_frequency_min_hertz.set(File.read(scaling_min_freq_path).to_f, [cpu_id])
+          end
+        end
+
+        def cmetrics
+          {
+            frequency_hertz: @frequency_hertz,
+            frequency_max_hertz: @frequency_max_hertz,
+            frequency_min_hertz: @frequency_min_hertz,
+            scaling_frequency_hertz: @scaling_frequency_hertz,
+            scaling_frequency_max_hertz: @scaling_frequency_max_hertz,
+            scaling_frequency_min_hertz: @scaling_frequency_min_hertz
+          }
+        end
+      end
+    end
+  end
+end
---
+++ test/plugin/test_cpufreq_collector.rb
@@ -0,0 +1,57 @@
+require "helper"
+require "fluent/plugin/in_node_exporter_metrics"
+require "fluent/plugin/node_exporter/cpufreq_collector"
+
+class NodeExporterCpufreqColectorTest < Test::Unit::TestCase
+  setup do
+    Fluent::Test.setup
+    @capability = Fluent::Capability.new
+  end
+
+  teardown do
+    Fluent::Engine.stop
+  end
+
+  CONFIG = config_element("ROOT", "", {
+                            "scrape_interval" => 5,
+                            "procfs_path" => "/proc",
+                            "sysfs_path" => "/sys"
+                          })
+
+  def create_driver(conf = CONFIG)
+    Fluent::Test::Driver::Input.new(Fluent::Plugin::NodeExporterMetricsInput).configure(conf)
+  end
+
+  sub_test_case "cpufreq" do
+    def test_frequency
+      config = {
+        sysfs_path: fixture_sysfs_root("cpufreq")
+      }
+      collector = Fluent::Plugin::NodeExporter::CpufreqMetricsCollector.new(config)
+      collector.run
+      frequency_hertz = collector.cmetrics[:frequency_hertz]
+      frequency_max_hertz = collector.cmetrics[:frequency_max_hertz]
+      frequency_min_hertz = collector.cmetrics[:frequency_min_hertz]
+      scaling_frequency_hertz = collector.cmetrics[:scaling_frequency_hertz]
+      scaling_frequency_max_hertz = collector.cmetrics[:scaling_frequency_max_hertz]
+      scaling_frequency_min_hertz = collector.cmetrics[:scaling_frequency_min_hertz]
+      assert_equal([2200000.0, 2500000.0, 2000000.0,
+                    2300000.0, 2400000.0, 2100000.0,
+                    2600000.0, 2900000.0, 2400000.0,
+                    2900000.0, 3000000.0, 2700000.0],
+                   [frequency_hertz.val(["0"]),
+                    frequency_max_hertz.val(["0"]),
+                    frequency_min_hertz.val(["0"]),
+                    frequency_hertz.val(["1"]),
+                    frequency_max_hertz.val(["1"]),
+                    frequency_min_hertz.val(["1"]),
+                    scaling_frequency_hertz.val(["0"]),
+                    scaling_frequency_max_hertz.val(["0"]),
+                    scaling_frequency_min_hertz.val(["0"]),
+                    scaling_frequency_hertz.val(["1"]),
+                    scaling_frequency_max_hertz.val(["1"]),
+                    scaling_frequency_min_hertz.val(["1"]),
+                   ])
+    end
+  end
+end
