0033f9d2597c9be3680e4022b3ce464c0992b75e
Kentaro Hayashi <hayashi@clear-code.com>
2021-09-13 14:35:00 +0900

    diskstats: add diskstats collector

---
+++ lib/fluent/plugin/node_exporter/diskstats_collector.rb
@@ -0,0 +1,268 @@
+#
+# Copyright (C) 2021- Kentaro Hayashi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require "cmetrics"
+require "fluent/plugin/input"
+require "fluent/plugin/node_exporter/collector"
+
+module Fluent
+  module Plugin
+    module NodeExporter
+      class DiskstatsMetricsCollector < MetricsCollector
+
+        IGNORED_DEVICES = /^(ram|loop|fd|(h|s|v|xv)d[a-z]|nvme\\d+n\\d+p)\\d+$/
+        # https://www.kernel.org/doc/Documentation/ABI/testing/procfs-diskstats
+        # major number ... time spent flushing
+        DISKSTATS_KNOWN_FIELDS = 20
+        # assume sector size = 512
+        SECTOR_SIZE = 512
+
+        METRIC_NAMES = %w(
+                reads_completed_total
+                reads_merged_total
+                read_bytes_total
+                read_time_seconds_total
+                writes_completed_total
+                writes_merged_total
+                written_bytes_total
+                write_time_seconds_total
+                io_now
+                io_time_seconds_total
+                io_time_weighted_seconds_total
+                discards_completed_total
+                discards_merged_total
+                discarded_sectors_total
+                discard_time_seconds_total
+                flush_requests_total
+                flush_requests_time_seconds_total
+        )
+
+        DISCARD_METRIC_NAMES = %w(
+               discards_completed_total
+               discards_merged_total
+               discarded_sectors_total
+               discard_time_seconds_total
+        )
+
+        FLUSH_METRIC_NAMES = %w(
+               flush_requests_total
+               flush_requests_time_seconds_total
+        )
+
+        def initialize(config={})
+          super(config)
+
+          @reads_completed_total = CMetrics::Counter.new
+          @reads_completed_total.create("node", "disk", "reads_completed_total",
+                                        "The total number of reads completed successfully.",
+                                        ["device"])
+
+          @reads_merged_total = CMetrics::Counter.new
+          @reads_merged_total.create("node", "disk", "reads_merged_total",
+                                     "The total number of reads merged.",
+                                     ["device"])
+
+          @read_bytes_total = CMetrics::Counter.new
+          @read_bytes_total.create("node", "disk", "read_bytes_total",
+                                   "The total number of bytes read successfully.",
+                                   ["device"])
+
+          @read_time_seconds_total = CMetrics::Counter.new
+          @read_time_seconds_total.create("node", "disk", "reads_time_seconds_total",
+                                          "The total number of seconds spent by all reads.",
+                                          ["device"])
+
+          @writes_completed_total = CMetrics::Counter.new
+          @writes_completed_total.create("node", "disk", "writes_completed_total",
+                                      "The total number of writes completed successfully.",
+                                      ["device"])
+
+          @writes_merged_total = CMetrics::Counter.new
+          @writes_merged_total.create("node", "disk", "writes_merged_total",
+                                      "The number of writes merged.",
+                                      ["device"])
+
+          @written_bytes_total = CMetrics::Counter.new
+          @written_bytes_total.create("node", "disk", "written_bytes_total",
+                                      "The total number of bytes written successfully.",
+                                      ["device"])
+
+          @write_time_seconds_total = CMetrics::Counter.new
+          @write_time_seconds_total.create("node", "disk", "write_time_seconds_total",
+                                           "This is the total number of seconds spent by all writes.",
+                                           ["device"])
+
+          @io_now = CMetrics::Gauge.new
+          @io_now.create("node", "disk", "io_now",
+                         "The number of I/Os currently in progress.",
+                         ["device"])
+
+          @io_time_seconds_total = CMetrics::Counter.new
+          @io_time_seconds_total.create("node", "disk", "io_time_seconds_total",
+                                        "Total seconds spent doing I/Os.",
+                                        ["device"])
+
+          @io_time_weighted_seconds_total = CMetrics::Counter.new
+          @io_time_weighted_seconds_total.create("node", "disk", "io_time_weighted_seconds_total",
+                                                 "The number of I/Os currently in progress.",
+                                                 ["device"])
+
+          if kernel_version_over4_18?
+            # Kernel >= 4.18
+            @discards_completed_total = CMetrics::Counter.new
+            @discards_completed_total.create("node", "disk", "discards_completed_total",
+                                             "The total number of discards completed successfully.",
+                                             ["device"])
+
+            @discards_merged_total = CMetrics::Counter.new
+            @discards_merged_total.create("node", "disk", "discards_merged_total",
+                                          "The total number of discards merged.", ["device"])
+
+            @discarded_sectors_total = CMetrics::Counter.new
+            @discarded_sectors_total.create("node", "disk", "discards_sectors_total",
+                                            "The total number of sectors discarded successfully.",
+                                            ["device"])
+
+            @discard_time_seconds_total = CMetrics::Counter.new
+            @discard_time_seconds_total.create("node", "disk", "discard_time_seconds_total",
+                                               "The total number of seconds spent by all discards.",
+                                               ["device"])
+          end
+
+          if kernel_version_over5_5?
+            @flush_requests_total = CMetrics::Counter.new
+            @flush_requests_total.create("node", "disk", "flush_requests_total",
+                                         "The total number of flush requests completed successfully",
+                                         ["device"])
+
+            @flush_requests_time_seconds_total = CMetrics::Counter.new
+            @flush_requests_time_seconds_total.create("node", "disk", "flush_requests_time_seconds_total",
+                                                      "This is the total number of seconds spent by all flush requests.",
+                                                      ["device"])
+          end
+
+          @metrics = {}
+        end
+
+        def kernel_version_over4_18?
+          Gem::Version.new(Etc.uname[:release].split('-', 2).first) >= Gem::Version.new("4.18")
+        end
+
+        def kernel_version_over5_5?
+          Gem::Version.new(Etc.uname[:release].split('-', 2).first) >= Gem::Version.new("5.5.0")
+        end
+
+        def run
+          diskstats_update
+        end
+
+        def diskstats_update
+          diskstats_path = File.join(@procfs_path, "diskstats")
+          File.readlines(diskstats_path).each do |line|
+            _, _, device,
+            reads_completed_value, reads_merged_value, read_bytes_value, read_time_seconds_value,
+            writes_completed_value, writes_merged_value, written_bytes_value, write_time_seconds_value,
+            io_now_value, io_time_seconds_value, io_time_weighted_seconds_value,
+            discards_completed_value, discards_merged_value, discarded_sectors_value, discard_time_seconds_value,
+            flush_requests_value, flush_requests_time_seconds_value = line.split(' ', DISKSTATS_KNOWN_FIELDS)
+            unless IGNORED_DEVICES.match?(device)
+              METRIC_NAMES.each do |field|
+                case field
+                when "reads_completed_total"
+                  @reads_completed_total.set(reads_completed_value.to_f, [device])
+                when "reads_merged_total"
+                  @reads_merged_total.set(reads_merged_value.to_f, [device])
+                when "read_bytes_total"
+                  @read_bytes_total.set(read_bytes_value.to_f * SECTOR_SIZE, [device])
+                when "read_time_seconds_total"
+                  @read_time_seconds_total.set(read_time_seconds_value.to_f * 0.001, [device])
+                when "writes_completed_total"
+                  @writes_completed_total.set(writes_completed_value.to_f, [device])
+                when "writes_merged_total"
+                  @writes_merged_total.set(writes_merged_value.to_f, [device])
+                when "written_bytes_total"
+                  @written_bytes_total.set(written_bytes_value.to_f * SECTOR_SIZE, [device])
+                when "write_time_seconds_total"
+                  @write_time_seconds_total.set(write_time_seconds_value.to_f * 0.001, [device])
+                when "io_now"
+                  @io_now.set(io_now_value.to_f, [device])
+                when "io_time_seconds_total"
+                  @io_time_seconds_total.set(io_time_seconds_value.to_f * 0.001, [device])
+                when "io_time_weighted_seconds_total"
+                  @io_time_weighted_seconds_total.set(io_time_weighted_seconds_value.to_f * 0.001, [device])
+                when "discards_completed_total"
+                  if kernel_version_over4_18?
+                    @discards_completed_total.set(discards_completed_value.to_f, [device])
+                  end
+                when "discards_merged_total"
+                  if kernel_version_over4_18?
+                    @discards_merged_total.set(discards_merged_value.to_f, [device])
+                  end
+                when "discarded_sectors_total"
+                  if kernel_version_over4_18?
+                    @discarded_sectors_total.set(discarded_sectors_value.to_f, [device])
+                  end
+                when "discard_time_seconds_total"
+                  if kernel_version_over4_18?
+                    @discard_time_seconds_total.set(discard_time_seconds_value.to_f * 0.001, [device])
+                  end
+                when "flush_requests_total"
+                  if kernel_version_over5_5?
+                    @flush_requests_total.set(flush_requests_value.to_f, [device])
+                  end
+                when "flush_requests_time_seconds_total"
+                  if kernel_version_over5_5?
+                    @flush_requests_time_seconds_total.set(flush_requests_time_seconds_value.to_f * 0.001, [device])
+                  end
+                end
+              end
+              @metrics = {
+                reads_completed_total: @reads_completed_total,
+                reads_merged_total: @reads_merged_total,
+                read_bytes_total: @read_bytes_total,
+                read_time_seconds_total: @read_time_seconds_total,
+                writes_completed_total: @writes_completed_total,
+                writes_merged_total: @writes_merged_total,
+                written_bytes_total: @written_bytes_total,
+                write_time_seconds_total: @write_time_seconds_total,
+                io_now: @io_now,
+                io_time_seconds_total: @io_time_seconds_total,
+                io_time_weighted_seconds_total: @io_time_weighted_seconds_total
+              }
+              if kernel_version_over4_18?
+                @metrics.merge!({
+                                  discards_completed_total: @discards_completed_total,
+                                  discards_merged_total: @discards_merged_total,
+                                  discarded_sectors_total: @discarded_sectors_total,
+                                  discard_time_seconds_total: @discard_time_seconds_total
+                               })
+              end
+              if kernel_version_over5_5?
+                @metrics.merge!({
+                                  flush_requests_total: @flush_requests_total,
+                                  flush_requests_time_seconds_total: @flush_requests_time_seconds_total
+                                })
+              end
+            end
+          end
+        end
+
+        def cmetrics
+          @metrics
+        end
+      end
+    end
+  end
+end
---
+++ test/plugin/test_diskstats_collector.rb
@@ -0,0 +1,125 @@
+require "helper"
+require "fluent/plugin/in_node_exporter_metrics"
+require "fluent/plugin/node_exporter/diskstats_collector"
+
+class DiskstatsColectorTest < Test::Unit::TestCase
+
+  DUMMY_DISKSTATS = <<EOS
+ 1000       0 nvme0n1 4000 5000 6000 7000 8000 9000 10000 11000 12000 13000 14000 15000 16000 17000 18000 19000 20000
+EOS
+
+  def parse(input)
+    stub(File).readlines { input.split("\n") }
+    collector = Fluent::Plugin::NodeExporter::DiskstatsMetricsCollector.new
+    collector.run
+    yield collector
+  end
+
+  sub_test_case "diskstats" do
+    def test_minimum_metrics
+      omit "/proc/diskstats is only available on *nix" if Fluent.windows?
+
+      # specify lower version to exclude discard and flush metrics explicitly
+      stub(Etc).uname { {release: "2.4.20-1-amd64"} }
+      parse(DUMMY_DISKSTATS) do |collector|
+        expected = (Fluent::Plugin::NodeExporter::DiskstatsMetricsCollector::METRIC_NAMES -
+                    Fluent::Plugin::NodeExporter::DiskstatsMetricsCollector::DISCARD_METRIC_NAMES -
+                    Fluent::Plugin::NodeExporter::DiskstatsMetricsCollector::FLUSH_METRIC_NAMES).collect { |v| v.intern }
+        assert_equal(expected, collector.cmetrics.keys)
+      end
+    end
+
+    def test_minimum_values
+      omit "/proc/diskstats is only available on *nix" if Fluent.windows?
+
+      # specify lower version to exclude discard and flush metrics explicitly
+      stub(Etc).uname { {release: "2.4.20-1-amd64"} }
+      parse(DUMMY_DISKSTATS) do |collector|
+        assert_equal([4000.0, 5000.0, 6000 * 512, 7.0,
+                      8000.0, 9000.0, 10000 * 512, 11.0,
+                      12000.0, 13.0, 14.0],
+                     [collector.cmetrics[:reads_completed_total].val(["nvme0n1"]),
+                      collector.cmetrics[:reads_merged_total].val(["nvme0n1"]),
+                      collector.cmetrics[:read_bytes_total].val(["nvme0n1"]),
+                      collector.cmetrics[:read_time_seconds_total].val(["nvme0n1"]),
+                      collector.cmetrics[:writes_completed_total].val(["nvme0n1"]),
+                      collector.cmetrics[:writes_merged_total].val(["nvme0n1"]),
+                      collector.cmetrics[:written_bytes_total].val(["nvme0n1"]),
+                      collector.cmetrics[:write_time_seconds_total].val(["nvme0n1"]),
+                      collector.cmetrics[:io_now].val(["nvme0n1"]),
+                      collector.cmetrics[:io_time_seconds_total].val(["nvme0n1"]),
+                      collector.cmetrics[:io_time_weighted_seconds_total].val(["nvme0n1"])
+                     ])
+      end
+    end
+  end
+
+  sub_test_case "discards metrics" do
+    def test_with_discards
+      # over 4.18 contains discard metrics
+      stub(Etc).uname { {release: "4.18.0-1-amd64"} }
+      parse(DUMMY_DISKSTATS) do |collector|
+        metric_exists = Fluent::Plugin::NodeExporter::DiskstatsMetricsCollector::DISCARD_METRIC_NAMES.all? do |v|
+          collector.cmetrics.keys.include?(v.intern)
+        end
+        assert_true(metric_exists)
+      end
+    end
+
+    def test_without_discards
+      # lower than 4.18 do not contain it
+      stub(Etc).uname { {release: "2.4.20-1-amd64"} }
+      parse(DUMMY_DISKSTATS) do |collector|
+        no_metric = Fluent::Plugin::NodeExporter::DiskstatsMetricsCollector::DISCARD_METRIC_NAMES.none? do |v|
+          collector.cmetrics.keys.include?(v.intern)
+        end
+        assert_true(no_metric)
+      end
+    end
+
+    def test_discard_values
+      stub(Etc).uname { {release: "4.18.0-1-amd64"} }
+      parse(DUMMY_DISKSTATS) do |collector|
+        assert_equal([15000.0, 16000.0, 17000.0, 18.0],
+                     [collector.cmetrics[:discards_completed_total].val(["nvme0n1"]),
+                      collector.cmetrics[:discards_merged_total].val(["nvme0n1"]),
+                      collector.cmetrics[:discarded_sectors_total].val(["nvme0n1"]),
+                      collector.cmetrics[:discard_time_seconds_total].val(["nvme0n1"])])
+      end
+    end
+  end
+
+  sub_test_case "flush metrics" do
+    def test_with_flush
+      # over 5.5 contains flush metrics
+      stub(Etc).uname { {release: "5.5.0-0-amd64"} }
+      parse(DUMMY_DISKSTATS) do |collector|
+        metric = Fluent::Plugin::NodeExporter::DiskstatsMetricsCollector::FLUSH_METRIC_NAMES.all? do |v|
+          collector.cmetrics.keys.include?(v.intern)
+        end
+        assert_true(metric)
+      end
+    end
+
+    def test_without_flush
+      # lower than 5.5 do not contain flush metrics
+      stub(Etc).uname { {release: "5.4.0-8-amd64"} }
+      parse(DUMMY_DISKSTATS) do |collector|
+        no_metric = Fluent::Plugin::NodeExporter::DiskstatsMetricsCollector::FLUSH_METRIC_NAMES.none? do |v|
+          collector.cmetrics.keys.include?(v.intern)
+        end
+        assert_true(no_metric)
+      end
+    end
+
+    def test_flush_values
+      stub(Etc).uname { {release: "5.5.0-1-amd64"} }
+      parse(DUMMY_DISKSTATS) do |collector|
+        assert_equal([19000.0, 20.0],
+                     [collector.cmetrics[:flush_requests_total].val(["nvme0n1"]),
+                      collector.cmetrics[:flush_requests_time_seconds_total].val(["nvme0n1"])])
+      end
+    end
+
+  end
+end
